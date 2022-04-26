1be0ecfb9479b853cfcde7c845b73e4f33a9f4d6
Josh Hendricks <jhendricks123@gmail.com>
2022-04-25 20:59:34 -0700

    maint: minor refactor for pester extension support

--- test.ps1
+++ test.ps1
@@ -1,18 +1,17 @@
 Import-Module Pester -MinimumVersion 5.2
-$container = New-PesterContainer -Path $PSScriptRoot\Certificate.tests.ps1 -Data @{ Urls = Import-Csv .\urls.csv }
+$container = New-PesterContainer -Path $PSScriptRoot\Certificate.tests.ps1
 $cfg = [PesterConfiguration]@{
-Run=@{
-    Container = $container
-    SkipRemainingOnFailure = $true
-    Exit=$true
+    Run        = @{
+        Container              = $container
+        SkipRemainingOnFailure = $true
+        Exit                   = $true
+    }
+    TestResult = @{
+        Enabled      = $true
+        OutputFormat = 'JUnitXml'
+    }
+    Output     = @{
+        Verbosity = 'Detailed'
+    }
 }
-TestResult = @{
-    Enabled = $true
-    OutputFormat = 'JUnitXml'
-}
-Output = @{
-    Verbosity = 'Detailed'
-}
-}
-Remove-Item testResults.xml -ErrorAction Ignore
 Invoke-Pester -Configuration $cfg
